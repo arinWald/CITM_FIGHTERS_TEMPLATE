44b052d93de04741b5f04bcfc671f4183ac92962
arinWald <arnau.gonzalez.acosta@estudiantat.upc.edu>
2024-01-05 16:20:48 +0100

    adas

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform player1;
-    public Transform player2;
+    public Transform player1pos;
+    public Transform player2pos;
     public float cameraMoveSpeed = 5f;
     public float minDistance = 2f;
     public float maxDistance = 10f;
@@ -15,8 +16,19 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
+        if (player1pos == null)
+        {
+            player1pos = GameObject.FindGameObjectWithTag("Azri").transform;
+        }
+
+        if (player2pos == null)
+        {
+            player2pos = GameObject.FindGameObjectWithTag("Thor").transform;
+        }
+
         // Calculate distance between players
-        float distanceBetweenPlayers = Vector3.Distance(player1.position, player2.position);
+        float distanceBetweenPlayers = Vector3.Distance(player1pos.position, player2pos.position);
+        Debug.Log(distanceBetweenPlayers);
 
         // Adjust camera position based on distance
         float targetZPosition = Mathf.Clamp(distanceBetweenPlayers, minDistance, maxDistance);
